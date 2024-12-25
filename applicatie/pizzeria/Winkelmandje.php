5bdb50429d5bcc96df421b8b7f49eab9eef30b72
MxYz1101 <m.yousufzai@student.han.nl>
2024-12-25 10:14:43 +0100

    quick fix

--- applicatie/pizzeria/Winkelmandje.php
+++ applicatie/pizzeria/Winkelmandje.php
@@ -1,18 +1,4 @@
 <!--PAGINA MET GEKOZEN ARTIKELEN OM DE BESTELLING TE BEVESTIGEN EN BETALEN-->
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizzeria Sole Machina</title>
-
-    <link rel="stylesheet" href="css/stylesheet.css">
-</head>
-
-<body><!--PAGINA MET GEKOZEN ARTIKELEN OM DE BESTELLING TE BEVESTIGEN EN BETALEN-->
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -94,35 +80,5 @@
     <footer>
             <a href="Privacyverklaring.php"> link naar privacy verklaring.</a>
     </footer>
-</body>
-</html>
-    <?php include 'functies/Header.php'; ?>
-
-    <main>
-        <div class="container">
-            <h2>Winkelmandje </h2>
-            <table>
-                <tr>
-                    <th>[$naam]</th>
-                    <th>[$Prijs]</th>
-                    <th>[$Aantal]</th>
-                    <th>[$Totaal]</th>
-                </tr>
-            </table>
-            <form>
-                <label for="address">Afleveradres:</label>
-                <input type="text" id="address" name="address" required><br><br>
-                <!-- Optie voor ophalen ??-->
-                <button type="submit">Bestelling Plaatsen</button>
-            </form>
-            <!--<h6>Winkelmand: </h6>
-            <h6>Anuleer artikel: </h6>
-            <h6>Anuleer bestelling: </h6>
-            <h6> Bestelling ophalen/wordt bezorgt naar adres..</h6>
-            <h6>Bevestig bestelling: Betalen</h6>-->
-        </div>
-    </main>
-
-    <?php include 'functies/Footer.php'; ?>
 </body>
 </html>
